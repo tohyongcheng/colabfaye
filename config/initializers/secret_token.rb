784c97ce01c47bbaa84e0aee2243a2a54ff443a6
Toh Yong Cheng <tohyongcheng@gmail.com>
2012-10-01 00:52:39 +0800

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fayecolab::Application.config.secret_token = '30944dfca34815c711c488b7def91615d0554e210801879bf58e4b7722b980a3abffab4d74cea47adc6c9dd22a54944ef88125592187622f4a086992190802bb'
